1b81836295206d142218a486ab068f2b23bc2c08
黃升煌 Mike <mike@miniasp.com>
2018-09-28 10:52:01 +0800

    任務05: 練習事件繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  count = 0;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  buttonClick(clickEvent) {
+    ++this.count;
+    console.log(clickEvent);
   }
 
 }
